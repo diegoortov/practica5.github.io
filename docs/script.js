d808cabd6058b9ff3a652ee549b1e9ead6c3bc00
diegoortov <diegoortizt2008@gmail.com>
2025-10-03 14:17:08 -0700

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,16 @@
+const imagenElemento = document.getElementById('carrusel-imagen');
+const imagenes = [
+    'img/imagen1.jpeg',
+    'img/imagen2.jpeg',
+    'img/imagen3.jpeg',
+];
+let indiceActual = 0;
+function cambiarImagen() {
+    imagenElemento.style.opacity = 0;
+    setTimeout(() => {
+        indiceActual = (indiceActual + 1) % imagenes.length;
+        imagenElemento.src = imagenes[indiceActual];
+        imagenElemento.style.opacity = 1;
+    }, 500);
+}
+setInterval(cambiarImagen, 3000);
